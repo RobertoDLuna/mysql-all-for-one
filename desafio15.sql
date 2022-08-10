d3edeb0606246af006b98bd31f1301d1a0cd02a8
Roberto Luna <robertocgw@gmail.com>
2022-08-10 17:57:40 -0300

    feat: Requisito 15 - Mostra somente as horas (sem os minutos e os segundos) da coluna submitted_date de todos registros da tabela purchase_orders.

---
+++ desafio15.sql
@@ -0,0 +1,5 @@
+# Requisito 15 - Mostra somente as horas (sem os minutos e os segundos) da coluna submitted_date de todos registros da tabela purchase_orders.
+
+SELECT HOUR(submitted_date)
+AS 'submitted_hour'
+FROM northwind.purchase_orders;
