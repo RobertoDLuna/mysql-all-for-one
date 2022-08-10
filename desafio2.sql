5254d71a970efe39c0aa1f9fb5546e217654ad00
Roberto Luna <robertocgw@gmail.com>
2022-08-10 15:56:14 -0300

    feat: Requisito 3 - exibe os valores da coluna que representa a primary key da tabela fix: requisito 2 padronizando a syntax

--- desafio2.sql
+++ desafio2.sql
@@ -1,3 +1,4 @@
 # Requisito 2 - Exibe os dados de todas as colunas da tabela 'products'
 
-SELECT * FROM northwind.products;
+SELECT *
+FROM `northwind`.`products`;
---
+++ desafio3.sql
@@ -0,0 +1,4 @@
+# Requisito 3 - exibe os valores da coluna que representa a primary key da tabela
+
+SELECT id
+FROM `northwind`.`products`;
