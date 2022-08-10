55b1f1e99e10b00963da93bc4d73ac3b9a425496
Roberto Luna <robertocgw@gmail.com>
2022-08-10 19:05:22 -0300

    feat: Atualize os dados da coluna discount da tabela order_details para 45, onde o valor na coluna unit_price seja maior que 10.0000 e o id seja um número entre 30 e 40.

---
+++ desafio24.sql
@@ -0,0 +1,5 @@
+# Requisito 24 - Atualize os dados da coluna discount da tabela order_details para 30, onde o valor na coluna unit_price seja menor que 10.0000.
+
+UPDATE northwind.order_details
+SET discount = 45
+WHERE unit_price < 10.0000 AND id BETWEEN 30 AND 40;
